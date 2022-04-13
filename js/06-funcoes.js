a61740d12adce724e3d8d869f51545e174012ecb
leonardo <leorsousa05@gmail.com>
2022-04-13 14:18:11 -0300

    funções

--- js/06-funcoes.js
+++ js/06-funcoes.js
@@ -1,4 +1,4 @@
-console.log("funções");
+'use strict'; // MODO ESTRITO/RESTRITO
 
 /*  Funções são blocos de código que
 podem ser executados e reaproveitados em
@@ -12,8 +12,6 @@ function linha() {
     console.log("----------------");
 }
 
-export { linha};
-
 let curso = "Programador Web";
 let escola = "Senac";
 let ano = 2022;
@@ -24,10 +22,20 @@ console.log(`Escola: ${escola}`);
 linha()
 console.log(`Ano ${ano}`);
 
+// VERSÃO 1: sintaxe tradicional
+/* function soma(value1, value2); {
+    let total = value1 + value2;
+    return total;
+}; */
 
-function soma(value1, value2); {
-    let total = value1 + value2
+// VERSÃO 2: sintaxe ARROW FUNCTION (FUNÇÃO SETA/FLECHA)
+const soma = (value1, value2) => {
+    let total = value1 + value2;
+    return total;
 };
+
+// Arrow Function com retorn IMPLÍCITO
+const soma1 = (value1, value2) => value1 + value2;
 
 console.log(soma(10,5));
 
@@ -37,3 +45,45 @@ console.log(resultado);
 if(resultado < 1000){
     console.log("Abaixo da meta")
 }
+
+linha();
+
+// VERSÃO 1: tradicional
+// function dobra(numero) {
+//     let resultado = numero * 2;
+//     return resultado;
+// }
+
+/* function dobra(numero) {
+    return numero * 2;
+}; */
+
+// VERSÃO 2: 
+/* const dobra = (numero) => {
+    let resultado = numero * 2;
+    return resultado;
+}; */
+
+/*
+const dobra = (numero) => {
+    return numero * 2
+}; */
+
+// Retorno IMPLÍCITO
+
+const dobra = numero => numero *2;
+
+console.log(dobra(10));
+console.log(dobra(150));
+
+linha();
+
+let nota1 = 8.5,
+    nota2 = 7.4;
+
+const calculamedia = (n1, n2) => (n1 + n2) / 2;
+
+console.log( calculamedia(nota1, nota2))
+
+// Obs.: se não tem parâmetros, também precisa usar ()
+const linha2 = () => console.log("*****")
